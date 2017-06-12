fcb16139e71e6e4d7536ae61b5bb52beb1ede020
jasonwhat <Jason@JASONs-MacBook.local>
2017-06-12 12:09:23 -0400

    changing font
    Changed font from Lora to Merriweather for for text and Raleway for
    headlines and H tags.

---
+++ assets/js/readMore.js
@@ -0,0 +1,59 @@
+$.fn.readMore = function(params){
+
+    return this.each(function(){
+
+      var $this = [];
+
+      $this.values = $.extend(
+                    {
+                       childTag : "p",
+                       minVisible : 2,
+                       excludeTag : "blockquote",
+                       minTextLengthToHide : 80,
+                       readMoreOpenedText : "- show less",
+                       readMoreClosedText : "+ uncover story",
+                       readMoreBtnClass : "uncover",
+                       animationSpeed : 500
+                     },params);
+
+  		$this.excludeTagStr = "";
+  		$this.is_visible = true;
+  		$this.total_element = $(this).children( $this.values.childTag ).size();
+  		$this.fisrtElement = $(this).children( $this.values.childTag ).eq( $this.values.minVisible - 1 );
+
+  		if($this.values.excludeTag.length > 0)
+  			    $this.excludeTagStr = ":not(" + $this.values.excludeTag + ")";
+
+  		$this.nextAllElement = $this.fisrtElement.nextAll( $this.excludeTagStr );
+
+  		$this.toogleAll = function() {
+                      if($this.fisrtElement.text().length < $this.values.minTextLengthToHide){
+                         $this.fisrtElement.slideToggle( $this.values.animationSpeed );
+                      }
+                      $this.nextAllElement.slideToggle( $this.values.animationSpeed );
+                    }
+
+  		$this.toogleAll();
+
+      $this.rdMoreId = "rd_more_" + parseInt(Math.random()*100000);
+
+
+  		if( $this.total_element > ($this.values.minVisible - 1) )
+  			  $(this).append( "<span style='cursor:pointer;' id = '" + $this.rdMoreId + "'class='rd_more " + $this.values.readMoreBtnClass + "'>+ uncover story</span>" );
+
+           $(this).find("#" + $this.rdMoreId).on("click" , function() {
+            if($this.is_visible){
+              $this.toogleAll();
+              $(this).html( $this.values.readMoreOpenedText );
+              $this.is_visible = false;
+            }
+            else{
+              $this.toogleAll();
+              $(this).html( $this.values.readMoreClosedText );
+              $this.is_visible = true;
+            }
+  		});
+
+      return $this;
+    });
+};
